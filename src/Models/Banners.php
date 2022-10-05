1b29bc1aa5e747efaeee4602ed22d6dc7467c706
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-06 01:49:46 +0700

    feat: Banners model

---
+++ src/Models/Banners.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace SaltBanner\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+use SaltFile\Traits\Fileable;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+
+class Banners extends Resources {
+    use Uuids;
+    use ObservableModel;
+    use Fileable;
+    protected $fileableFields = ['video', 'banner'];
+    protected $fileableCascade = true;
+    protected $fileableDirs = [
+        'video' => 'banners/video',
+        'banner' => 'banners/image',
+    ];
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table provinces
+        'id',
+        'title',
+        'description',
+        'status',
+        'type',
+        'videourl',
+        'order'
+    ];
+
+    protected $rules = array(
+        "title" => 'required|string',
+        "description" => 'required|string',
+        "status" => 'nullable|string',
+        "type" => 'nullable|string',
+        "videourl" => 'nullable|string',
+        "order" => 'required|integer',
+    );
+
+    protected $auths = array (
+        // 'index',
+        'store',
+        // 'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $structures = array();
+
+    protected $forms = array();
+    protected $searchable = array( 'title', 'description', 'status', 'type','videourl','order', 'link');
+    protected $fillable = array( 'title', 'description', 'status', 'type','videourl','order', 'link');
+
+    public function banner() {
+        return $this->hasOne('SaltFile\Models\Files', 'foreign_id', 'id')
+                    ->where('foreign_table', 'banners')
+                    ->where('directory', 'banners/image');
+    }
+
+    public function video() {
+        return $this->hasOne('SaltFile\Models\Files', 'foreign_id', 'id')
+                    ->where('foreign_table', 'banners')
+                    ->where('directory', 'banners/video');
+    }
+}
